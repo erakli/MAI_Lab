6d5530e498de28ae986a90459054faef38c00c8a
erakli-main <erakli00@yandex.com>
2016-10-29 18:04:27 +0300

    Removed Numbers namespace

--- TheFall/include/Functions.h
+++ TheFall/include/Functions.h
@@ -6,12 +6,8 @@
 /* * * * * Вспомогательные функции * * * * */
 namespace MyFunc
 {
-	namespace Numbers
-	{
-		// перевод из градусов в радианы
-		inline TYPE deg2rad(const TYPE &angle){
-			return angle * PI / 180;
-		};
-
-	}
+	// перевод из градусов в радианы
+	inline TYPE deg2rad(const TYPE &angle){
+		return angle * PI / 180;
+	};
 }
