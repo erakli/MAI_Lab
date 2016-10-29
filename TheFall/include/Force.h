2c73d1a99d09c75e1154370dde4dcc5f5b0cb19f
erakli-main <erakli00@yandex.com>
2016-10-29 20:49:12 +0300

    New abstract Force class

---
+++ TheFall/include/Force.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "Types.h"
+#include "LinearAlgebra.h"
+
+class Force
+{
+public:
+	virtual ~Force();
+
+	virtual Eigen::VectorXd getRight(const Eigen::VectorXd &X, TYPE t) const = 0;
+};
