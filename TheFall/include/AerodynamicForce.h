e1dc7f8a2f6c70d1dc47b39509d8f93f4afa251e
erakli-main <erakli00@yandex.com>
2016-10-31 01:04:24 +0300

    New AerodynamicForce class

---
+++ TheFall/include/AerodynamicForce.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "Force.h"
+
+class AerodynamicForce : public Force
+{
+public:
+	Eigen::Vector3d getRight(const Vector6d &X, TYPE t) const override;
+
+private:
+	TYPE GetDensity(const Eigen::Vector3d& X) const;
+};
---
+++ TheFall/src/AerodynamicForce.cpp
@@ -0,0 +1,36 @@
+﻿#include "AerodynamicForce.h"
+
+#include "Constants.h"
+#include <SolarSystem.h>
+
+using namespace Eigen;
+
+Vector3d AerodynamicForce::getRight(const Vector6d& X, TYPE t) const
+{
+	Vector3d pos, veloc;
+	pos = X.head(VEC_SIZE);
+	veloc = X.tail(VEC_SIZE);
+
+	Vector3d earth_ang_veloc(0.0, 0.0, Earth::angularVeloc);
+
+	Vector3d atmospheric_veloc;
+	atmospheric_veloc = veloc - earth_ang_veloc.cross(pos);
+
+	TYPE scalar_part = atmospheric_veloc.norm() * GetDensity(pos) / 2.0;
+
+	Vector3d right_part;
+	right_part = scalar_part * atmospheric_veloc;
+
+	return right_part;
+}
+
+
+
+TYPE AerodynamicForce::GetDensity(const Vector3d& X) const
+{
+	TYPE height = X.norm() - Earth::meanRadius;
+
+	TYPE density;
+
+	return density;
+}
