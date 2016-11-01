8f14a95f9b82e528e6a4916cc51d9607897c5e34
erakli-main <erakli00@yandex.com>
2016-11-02 02:45:55 +0300

    Bug fix
    Return vector type and return value wasn't equivalent

--- TheFall/src/Gravitation.cpp
+++ TheFall/src/Gravitation.cpp
@@ -6,11 +6,13 @@ using namespace Eigen;
 
 Vector3d GravitationField::getRight(const Vector6d &X, TYPE t) const
 {
+	Vector3d head = X.head(VEC_SIZE);
+
 	TYPE
-		module = X.head(VEC_SIZE).norm(),
+		module = head.norm(),
 		moduleX3 = pow(module, 3);
 
 	TYPE scalar_part = -Earth::muEarth / moduleX3;
 
-	return X * scalar_part;
+	return head * scalar_part;
 }
