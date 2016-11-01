84e1888c48392faa507e6e93e6717c7b5336ba14
erakli-main <erakli00@yandex.com>
2016-11-02 02:39:03 +0300

    DensityModelParams added

--- TheFall/include/AerodynamicForce.h
+++ TheFall/include/AerodynamicForce.h
@@ -2,11 +2,30 @@
 
 #include "Force.h"
 
+#define ATMO_LAYERS	8
+
+
+
+struct DensityModelParams
+{
+	TYPE
+		height,	// [м]
+		A,		// [кг/м^3]
+		k1,		// [м^-2]
+		k2;		// [м^-1]
+
+	DensityModelParams();
+};
+
+
+
 class AerodynamicForce : public Force
 {
 public:
+	AerodynamicForce();
 	Eigen::Vector3d getRight(const Vector6d &X, TYPE t) const override;
 
 private:
-	TYPE GetDensity(const Eigen::Vector3d& X) const;
+	DensityModelParams density_params[ATMO_LAYERS];
+	TYPE GetDensity(const Eigen::Vector3d& X, TYPE t) const;
 };
--- TheFall/src/AerodynamicForce.cpp
+++ TheFall/src/AerodynamicForce.cpp
@@ -5,6 +5,60 @@
 
 using namespace Eigen;
 
+
+
+DensityModelParams::DensityModelParams()
+{
+	height = 0.0;
+	A = 0.0;
+	k1 = 0.0;
+	k2 = 0.0;
+}
+
+
+
+AerodynamicForce::AerodynamicForce()
+{
+
+	density_params[0].height = 0.0;
+	density_params[1].height = 2.0e+4;
+	density_params[2].height = 6.0e+4;
+	density_params[3].height = 1.0e+5;
+	density_params[4].height = 1.5e+5;
+	density_params[5].height = 3.0e+5;
+	density_params[6].height = 6.0e+5;
+	density_params[7].height = 9.0e+5;
+
+	density_params[0].A = 1.225;
+	density_params[1].A = 0.891e-1;
+	density_params[2].A = 2.578e-4;
+	density_params[3].A = 4.061e-7;
+	density_params[4].A = 2.130e-9;
+	density_params[5].A = 4.764e-11;
+	density_params[6].A = 8.726e-13;
+	density_params[7].A = 6.367e-14;
+
+	density_params[0].k1 = -0.2639e-8;
+	density_params[1].k1 = 0.4407e-9;
+	density_params[2].k1 = -0.2560e-8;
+	density_params[3].k1 = 0.1469e-8;
+	density_params[4].k1 = 0.8004e-10;
+	density_params[5].k1 = 0.7111e-11;
+	density_params[6].k1 = 0.1831e-11;
+	density_params[7].k1 = 0;
+
+	density_params[0].k2 = 0.7825e-4;
+	density_params[1].k1 = 0.16375e-3;
+	density_params[2].k1 = 0.5905e-4;
+	density_params[3].k1 = 0.1787e-3;
+	density_params[4].k1 = 0.3734e-4;
+	density_params[5].k1 = 0.1547e-4;
+	density_params[6].k1 = 0.9280e-5;
+	density_params[7].k1 = 0.9540e-5;
+}
+
+
+
 Vector3d AerodynamicForce::getRight(const Vector6d& X, TYPE t) const
 {
 	Vector3d pos, veloc;
