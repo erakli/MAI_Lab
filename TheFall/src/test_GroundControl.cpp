2e4c667d741f2bab85c8aeed685a3289a78c166c
erakli-main <erakli00@yandex.com>
2016-11-09 01:25:29 +0300

    Test changed according to new funcs

--- TheFall/src/test_GroundControl.cpp
+++ TheFall/src/test_GroundControl.cpp
@@ -25,37 +25,41 @@ MatrixXd GenerateSputnikOrbit(TYPE duration);
 
 int main()
 {
-	TYPE duration = SECINDAY;
+	TYPE duration = SECINDAY / 2.0;
 
 	MatrixXd sputnik_orbit = GenerateSputnikOrbit(duration);
 
 	GroundStation ground_station;
-	ground_station._topo_position << 0, deg2rad(18), deg2rad(173);
+	ground_station._geographic_pos << 0, deg2rad(18), deg2rad(173);
 	ground_station._vision_zone_angle = deg2rad(150);
 	ground_station.observations.resize(int(ceil(duration) + 1), 2);
 
-	Vector3d cur_topo_position = ground_station._topo_position;
+	Vector3d cur_geographic_pos = ground_station._geographic_pos;
+//	MatrixXd fix_positions;
+//	fix_positions.resize(int(ceil(duration) + 1), 3);
 
 	Vector3d sputnik_fix_pos;
-	Vector3d sputnik_topo_pos;
-	Vector2d sputnik_horizontal_pos;
+	Vector2d sputnik_horiz_pos;
 
-	TYPE lambda_start = ground_station._topo_position(2);
-	TYPE start_time = 0;
+	TYPE lambda_start = ground_station._geographic_pos(2);
+	TYPE start_time = 0.0;
 
 	for (size_t sec = 0; sec < int(ceil(duration) + 1); sec++)
 	{
-		cur_topo_position(2) = lambda_start + StarTime(start_time, sec);
+		cur_geographic_pos(2) = lambda_start + StarTime(start_time, sec);
 
 		sputnik_fix_pos = sputnik_orbit.row(sec).segment(1, VEC_SIZE);
-		sputnik_topo_pos = Fix2Topo(sputnik_fix_pos, cur_topo_position);
+		sputnik_horiz_pos = Fix2Horiz(sputnik_fix_pos, cur_geographic_pos);
 
-		sputnik_horizontal_pos = Topo2Horiz(sputnik_topo_pos, cur_topo_position);
-		ground_station.observations.row(sec) = sputnik_horizontal_pos * (180.0 / PI);
+		// TODO: добавить проверку на попадание элевации в конус видимости НИП
+
+		ground_station.observations.row(sec) = sputnik_horiz_pos * (180.0 / PI);
+//		fix_positions.row(sec) = Geographic2Fix(cur_geographic_pos);
 	}
 
 	to_file(sputnik_orbit);
 	to_file(ground_station.observations);
+	//to_file(fix_positions);
 }
 
 
@@ -83,7 +87,7 @@ MatrixXd GenerateSputnikOrbit(TYPE duration)
 	TYPE e = (alpha_height - pi_height) / (alpha_height + pi_height);
 
 	Orbit::Kepler_elements
-		elements = { 0, deg2rad(42), 0, a, e, 0 };
+		elements = { 0, deg2rad(42), 0, a, e, deg2rad(180) };
 
 	DormanPrinceSolver Integrator;
 	GravitationField central_field;
