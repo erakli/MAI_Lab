6b4fe9ae62ef8afd1b6e3afdd786ea34e95adae6
erakli-Netbook <erakli00@yandex.com>
2016-11-10 16:55:46 +0300

    New transformation constants and funcs

--- TheFall/include/Functions.h
+++ TheFall/include/Functions.h
@@ -3,11 +3,19 @@
 #include "Types.h"
 #include "Constants.h"
 
+#define RAD_IN_DEG	0.01745329251994329576923690768489
+#define DEG_IN_RAD	57.295779513082320876798154814105
+
 /* * * * * Вспомогательные функции * * * * */
 namespace MyFunc
 {
 	// перевод из градусов в радианы
 	inline TYPE deg2rad(const TYPE &angle){
-		return angle * PI / 180;
+		return angle * RAD_IN_DEG;
+	};
+
+	// из радиан в градусы
+	inline TYPE rad2deg(const TYPE &angle){
+		return angle * DEG_IN_RAD;
 	};
 }
