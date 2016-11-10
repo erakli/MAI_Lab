ee20aa21be181ffcd01977943f6274d5862cd7a0
erakli-Netbook <erakli00@yandex.com>
2016-11-10 16:57:54 +0300

    Tech files changes

--- TheFall/src/test_SolarSystem.cpp
+++ TheFall/src/test_SolarSystem.cpp
@@ -1,8 +1,15 @@
 #include "SolarSystemTests.h"
 
+#include <iostream>
+using namespace std;
+
 int main()
 {
 	Tests::TestTime();
 	Tests::TestCoordinates();
+	Tests::TestFix2Horiz();
 	Tests::SolarSystem();
+
+	cout << "\nPress any key to exit...";
+	int a; cin >> a;
 }
