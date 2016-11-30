a33d6a066d23ab4cbc2a3046c886c04318bfc218
erakli-main <erakli00@yandex.com>
2016-11-30 04:40:16 +0300

    Changes on test

--- TheFall/src/test_GroundStation.cpp
+++ TheFall/src/test_GroundStation.cpp
@@ -19,6 +19,7 @@
 #include "LeastSquareMethod.h"
 
 #define LSM_TEST
+//#define TEST
 
 using namespace Eigen;
 using namespace MyFunc;
@@ -30,42 +31,65 @@ MatrixXd GenerateSputnikOrbit(TYPE duration);
 
 int main()
 {
-	TYPE duration = SECINDAY * 0.5;
+	TYPE duration = SECINDAY * 5;
 
 	TYPE stddev = 3.3 / 60.0; // перевели угловые минуты в градусы
 
 	MyNormalDistribution::param_type random_params(0.0, deg2rad(stddev));
 	DistributionParamVec random_param_vec({ random_params, random_params });
 
-	MatrixXd sputnik_orbit = GenerateSputnikOrbit(duration);
-	size_t num_of_results = sputnik_orbit.rows();
-
 	GroundStation ground_station(
 		{ 0, deg2rad(18), deg2rad(173) }, PI_HALF - deg2rad(75));
 
-	ground_station.SetRandomErrorParams(random_param_vec);
-	ground_station.SetDoRandom(true);
-	ground_station.Init(num_of_results);
+#ifdef LSM_TEST
+	VectorXd initial_condition;
+	VectorXd true_initial_condition;
+	ObservationSessionsVector observation_sessions_vec;
+#endif
 
-	Vector3d sputnik_fix_pos;
-
-	for (size_t sec = 0; sec < num_of_results; sec++)
 	{
-		sputnik_fix_pos = sputnik_orbit.row(sec).head(VEC_SIZE);
-		ground_station.SaveObservation(sputnik_fix_pos, sec, sec);
-	}
+		MatrixXd sputnik_orbit = GenerateSputnikOrbit(duration);
+		size_t num_of_results = sputnik_orbit.rows();
 
-	cout << endl << "Finished" << endl;
+		ground_station.SetRandomErrorParams(random_param_vec);
+		ground_station.SetDoRandom(true);
+		ground_station.Init(num_of_results);
+
+		Vector3d sputnik_fix_pos;
+
+		for (size_t sec = 0; sec < num_of_results; sec++)
+		{
+			sputnik_fix_pos = sputnik_orbit.row(sec).head(VEC_SIZE);
+			ground_station.SaveObservation(sputnik_fix_pos, sec, sec);
+		}
+
+		cout << endl << "Finished" << endl;
+
+		//cout << " * Saving sputnik_orbit" << endl;
+		//to_file(sputnik_orbit);
+		//to_file(modules);
+
+		//cout << " * Saving observations" << endl;
+		//to_file(ground_station.GetObservations(), false);
 
 #ifdef LSM_TEST
-	cout << endl << "Started LSM" << endl;
+		cout << endl << "Started LSM" << endl;
 
-	ObservationSessionsVector observation_sessions_vec = 
-		ground_station.GetObservationSessionsVector();
+		observation_sessions_vec =
+			ground_station.GetObservationSessionsVector();
 
-	VectorXd initial_condition =
-		sputnik_orbit.row(observation_sessions_vec.front().start_moment);
+		// берём вектор состояния на первый момент наблюдений и слегка варьируем
+		true_initial_condition =
+			sputnik_orbit.row(observation_sessions_vec.front().start_moment);
 
+		initial_condition = true_initial_condition.array() * 1.10;
+		//Orbit::Kepler_elements another_elements = Orbit::Decart2Kepler(true_initial_condition);
+		//another_elements.a += another_elements.a * 1.0e-2;
+		//initial_condition = Orbit::Kepler2Decart(another_elements);
+#endif
+	}
+
+#ifdef LSM_TEST
 	VectorXd observations_disp_vec(2);
 	observations_disp_vec << 
 		deg2rad(pow(stddev, 2)),
@@ -83,6 +107,7 @@ int main()
 	sputnik.AddForce(&central_field);
 	sputnik.AddForce(&aerodynamic_force);
 	sputnik.SetInterval(1.0);
+	sputnik.SetDisableStopCalculation(true);
 
 	GroundStation observation_model(ground_station);
 	observation_model.SetDoRandom(false);
@@ -95,23 +120,25 @@ int main()
 	ls_method.SetObservationModel(&observation_model);
 	ls_method.SetObservationSessionsVec(observation_sessions_vec);
 
-	MatrixXd log_matrix = ls_method.Run(3.0);
+	Vector6d stop_condition;
+	stop_condition <<  2.0, 2.0, 2.0, 0.1, 0.1, 0.1;
 
-	initial_condition = ls_method.GetInitialCondition();
+	MatrixXd log_matrix = ls_method.Run(stop_condition);
+
+	VectorXd res_initial_condition = ls_method.GetInitialCondition();
 
 	cout << " * Saving log_matrix" << endl;
 	to_file(log_matrix);
 
-	cout << " * Saving initial_condition" << endl;
-	to_file(initial_condition);
+	MatrixXd initial_together(3, initial_condition.size());
+	initial_together <<
+		true_initial_condition.transpose(),
+		initial_condition.transpose(),
+		res_initial_condition.transpose();
+
+	cout << " * Saving initial_conditions" << endl;
+	to_file(initial_together);
 #endif
-
-//	cout << " * Saving sputnik_orbit" << endl;
-//	to_file(sputnik_orbit);
-//	to_file(modules);
-
-	cout << " * Saving observations" << endl;
-	to_file(ground_station.GetObservations(), false);
 
 	system("pause");
 }
@@ -142,6 +169,17 @@ MatrixXd GenerateSputnikOrbit(TYPE duration)
 
 	Orbit::Kepler_elements
 		elements = { 0, deg2rad(42), 0, a, e, deg2rad(0) };
+
+#ifdef TEST
+	Vector6d X = Orbit::Kepler2Decart(elements);
+	Orbit::Kepler_elements another_elements = Orbit::Decart2Kepler(X);
+	Vector6d X2 = Orbit::Kepler2Decart(another_elements);
+
+	Information(elements);
+	cout << "Result X: " << X.transpose() << endl;
+	Information(another_elements);
+	cout << "Result X2: " << X2.transpose() << endl;
+#endif
 
 	DormanPrinceSolver solver;
 	GravitationField central_field;
