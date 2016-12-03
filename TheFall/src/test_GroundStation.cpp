a50bfbd1f526e5fc8fec3eeff78ec7c5757194e3
erakli-main <erakli00@yandex.com>
2016-12-04 00:32:57 +0300

    Many changes in test_GroundStation
    Important:
    - fixed observation error's variance value
    - found proper delta values for good results of LSM
    - changed stop_conditions
    
    Also, added new macros, added new universal GenerateSputnikOrbit func.
    Started statistical part (Monte-Carlo method).

--- TheFall/src/test_GroundStation.cpp
+++ TheFall/src/test_GroundStation.cpp
@@ -20,6 +20,7 @@
 
 #define LSM_TEST
 //#define TEST
+#define INITIALS
 
 using namespace Eigen;
 using namespace MyFunc;
@@ -28,12 +29,15 @@ using namespace std;
 
 void Information(const Orbit::Kepler_elements &elements);
 MatrixXd GenerateSputnikOrbit(TYPE duration);
+MatrixXd GenerateSputnikOrbit(const VectorXd& initial_conditions, TYPE t0, TYPE t1, bool with_time = false);
 
 int main()
 {
 	TYPE duration = SECINDAY * 2.0;
 
-	TYPE stddev = 3.3 / 60.0; // перевели угловые минуты в градусы
+	TYPE stddev =
+		//0.5;
+		3.3 / 60.0; // перевели угловые минуты в градусы
 
 	MyNormalDistribution::param_type random_params(0.0, deg2rad(stddev));
 	DistributionParamVec random_param_vec({ random_params, random_params });
@@ -52,7 +56,7 @@ int main()
 		size_t num_of_results = sputnik_orbit.rows();
 
 		ground_station.SetRandomErrorParams(random_param_vec);
-		ground_station.SetDoRandom(false);
+		ground_station.SetDoRandom(true);
 		ground_station.Init(num_of_results);
 
 		Vector3d sputnik_fix_pos;
@@ -74,18 +78,25 @@ int main()
 #ifdef LSM_TEST
 		cout << endl << "Started LSM" << endl;
 
-		observation_sessions_vec =
-			ground_station.GetObservationSessionsVector();
+		ObservationSession lsm_start_session = ground_station.GetObservationSessionsVector().front();
+
+		// берЄм только последнее измерение
+		observation_sessions_vec.assign(
+			1, lsm_start_session);
+
 
 		// берЄм вектор состо€ни€ на первый момент наблюдений и слегка варьируем
 		true_initial_condition =
-			sputnik_orbit.row(observation_sessions_vec.front().start_moment);
+			sputnik_orbit.row(lsm_start_session.start_moment);
 
 		initial_condition = true_initial_condition;
-		initial_condition(0) += 10.0;
-		initial_condition(1) += 10.0;
+		initial_condition(0) += 100.0;
+		initial_condition(1) -= 10.0;
+		initial_condition(2) -= 50.0;
 		//Orbit::Kepler_elements another_elements = Orbit::Decart2Kepler(true_initial_condition);
-		//another_elements.a += another_elements.a * 1.0e-2;
+		//another_elements.a += another_elements.a * 1.0e-1;
+		//another_elements.e -= another_elements.e * 1.0e-2;
+		//another_elements.i += another_elements.i * 1.0e-2;
 		//initial_condition = Orbit::Kepler2Decart(another_elements);
 #endif
 //	}
@@ -93,9 +104,9 @@ int main()
 #ifdef LSM_TEST
 	VectorXd observations_disp_vec(2);
 	observations_disp_vec <<
-		//deg2rad(pow(stddev, 2)),
-		//deg2rad(pow(stddev, 2));
-		1.0, 1.0;	// TODO: временно убрали ошибки измерений
+		pow(deg2rad(stddev), 2),
+		pow(deg2rad(stddev), 2);
+//		1.0, 1.0;	// TODO: временно убрали ошибки измерений
 
 	GravitationField central_field;
 	AerodynamicForce aerodynamic_force;
@@ -132,27 +143,37 @@ int main()
 
 	LeastSquareMethod ls_method;
 	ls_method.SetInitialCondition(initial_condition);
-	ls_method.SetObservations(ground_station.GetObservations());
+	// TODO: возьмЄм только последний участок измерений
+	ls_method.SetObservations(ground_station.GetObservations().topRows(lsm_start_session.GetDuration()));
 	ls_method.SetObservationsError(observations_disp_vec);
 	ls_method.SetModel(&sputnik);
 	ls_method.SetObservationModel(&observation_model);
 	ls_method.SetObservationSessionsVec(observation_sessions_vec);
 
+
+	TYPE pos_delta = 1.0e-10;
+	TYPE veloc_delta = 1.0e-10;
 	VectorXd delta(6);
-	delta << 0.5, 0.5, 0.5, 0.02, 0.02, 0.02;
+	delta << 
+		pos_delta, pos_delta, pos_delta, 
+		veloc_delta, veloc_delta, veloc_delta;
+
 	ls_method.SetDelta(delta);
 
 	Vector6d stop_condition;
-	stop_condition <<  2.0, 2.0, 2.0, 0.1, 0.1, 0.1;
+	stop_condition << 1.0e-3, 1.0e-3, 1.0e-3, 1.0e-3, 1.0e-3, 1.0e-3;
 	//TYPE stop_condition = 3.0;
 
 	MatrixXd log_matrix = ls_method.Run(stop_condition);
 
 	VectorXd res_initial_condition = ls_method.GetInitialCondition();
 
+#ifdef LOG_MATRIX
 	cout << " * Saving log_matrix" << endl;
 	to_file(log_matrix);
+#endif
 
+#ifdef INITIALS
 	MatrixXd initial_together(3, initial_condition.size());
 	initial_together <<
 		true_initial_condition.transpose(),
@@ -163,7 +184,84 @@ int main()
 	to_file(initial_together);
 #endif
 
-	system("pause");
+#ifdef EVOLUTION
+	MatrixXd initial_evolution;
+	initial_evolution.resizeLike(log_matrix);
+	initial_evolution.row(0) = log_matrix.row(0) + initial_condition.transpose() - true_initial_condition.transpose();
+
+	for (size_t i = 1; i < initial_evolution.rows(); i++)
+	{
+		initial_evolution.row(i) = initial_evolution.row(i - 1) + log_matrix.row(i);
+	}
+
+	cout << " * Saving initial_evolution" << endl;
+	to_file(initial_evolution);
+#endif
+
+	cout << endl << "Modelling fall of sputnik" << endl;
+
+	MatrixXd orbit_result;
+	MatrixXd pos_matrix;
+	TYPE t0 = lsm_start_session.start_moment;
+	TYPE t1 = t0 + SECINDAY * 30;
+	TYPE min;
+	size_t min_idx;
+	VectorXd min_vec;
+	Vector3d temp_vec;
+	Vector3d fall_coord;
+
+	MatrixXd fall_results(30, 3);
+	MatrixXd fall_fix(30, 3);
+	//MatrixXd fall_test(30, 3);
+	size_t num_of_fall = 0;
+
+	TYPE JD;
+
+	for (size_t i = 0; i < 30; i++)
+	{
+		cout << endl << "Num of falling - " << i << endl;
+
+		// со временем смоделируем
+		orbit_result = 
+			GenerateSputnikOrbit(res_initial_condition, t0, t1, true).bottomLeftCorner(15, VEC_SIZE + 1);
+		pos_matrix = orbit_result.rightCols(VEC_SIZE);
+
+		min_vec = pos_matrix.rowwise().stableNorm();
+		min_vec.array() -= Earth::meanRadius;
+		min = min_vec.cwiseAbs().minCoeff(&min_idx);
+
+		cout << "min_idx = " << min_idx << endl;
+		cout << "min_vec:" << endl << min_vec << endl;
+
+		temp_vec = pos_matrix.row(min_idx);
+		if (min > 0.5)
+			continue;
+
+		JD = 0.5 + orbit_result(min_idx, 0) / SECINDAY;
+
+		fall_fix.row(num_of_fall) = temp_vec;
+
+		fall_coord = Decart2Spher(temp_vec);
+		fall_results.row(num_of_fall) = Spher2Geographic(fall_coord, JD);
+
+		//fall_test.row(num_of_fall) = 
+		//	Geographic2Fix(fall_results.row(num_of_fall), JD);
+
+		num_of_fall++;
+
+		//to_file(orbit_result);
+		//break;
+	}
+
+	cout << " * Saving fall_results" << endl;
+	to_file(MatrixXd(fall_fix.topRows(num_of_fall)));
+	to_file(MatrixXd(fall_results.topRows(num_of_fall)));
+	//to_file(MatrixXd(fall_test.topRows(num_of_fall)));
+
+
+#endif
+
+//	system("pause");
 }
 
 
@@ -185,13 +283,15 @@ void Information(const Orbit::Kepler_elements &elements)
 MatrixXd GenerateSputnikOrbit(TYPE duration)
 {
 	TYPE alpha_height = 970 + Earth::meanRadius;
-	TYPE pi_height = 40 + Earth::meanRadius;
+	TYPE pi_height = 140 + Earth::meanRadius;
 
 	TYPE a = (alpha_height + pi_height) / 2.0;
 	TYPE e = (alpha_height - pi_height) / (alpha_height + pi_height);
 
 	Orbit::Kepler_elements
 		elements = { 0, deg2rad(42), 0, a, e, deg2rad(0) };
+
+	Information(elements);
 
 #ifdef TEST
 	//Vector6d X = Orbit::Kepler2Decart(elements);
@@ -204,20 +304,30 @@ MatrixXd GenerateSputnikOrbit(TYPE duration)
 	//cout << "Result X2: " << X2.transpose() << endl;
 #endif
 
+	return GenerateSputnikOrbit(Orbit::Kepler2Decart(elements), 0.0, duration);
+}
+
+
+
+MatrixXd GenerateSputnikOrbit(const VectorXd& initial_conditions, TYPE t0, TYPE t1, bool with_time)
+{
+	cout << endl << "GenerateSputnikOrbit()" << endl;
+
 	DormanPrinceSolver_fixed solver;
 	GravitationField central_field;
 	AerodynamicForce aerodynamic_force;
-	Sputnik sputnik(elements);
+	Sputnik sputnik;
 
-	Information(elements);
+	sputnik.SetStart(initial_conditions);
 
 	sputnik.SetMass(50);
 	sputnik.SetBallisticCoeff(1.4);
 
-	sputnik.Set_t1(duration);
+	sputnik.Set_t0(t0);
+	sputnik.Set_t1(t1);
 	sputnik.SetInterval(1.0);
 
-	aerodynamic_force.SetHasRandom(false);
+	aerodynamic_force.SetHasRandom(true);
 
 	sputnik.AddForce(&central_field);
 	sputnik.AddForce(&aerodynamic_force);
@@ -225,10 +335,10 @@ MatrixXd GenerateSputnikOrbit(TYPE duration)
 	// TODO: добавить установку интервала 
 	// корел€ции аэродинамической силы
 	solver.SetCorrelationInterval(1.0);
-//	solver.SetEpsMax(1.0e-13);
+	//	solver.SetEpsMax(1.0e-13);
 	solver.Run(sputnik);
 
-	cout << endl << "sputnik_orbit generated" << endl;
+	cout << "sputnik_orbit generated" << endl << endl;
 
-	return sputnik.GetResult(false);
+	return sputnik.GetResult(with_time);
 }
